8a6ab4e0837f989a21aabf1cf30fa4d6d993f885
Benny Luo <bennyluo95@gmail.com>
2022-01-04 22:20:39 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <Body>
+        <div className=''>
+          <h3>Hi, I'm Benny</h3>
+          <img>ProfilePic1</img>
+        </div>
+      </Body>
+    </div>
+  );
+}
+
+export default App;
