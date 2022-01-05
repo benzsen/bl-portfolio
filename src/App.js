2a6ded2ae879a967f9addf76abbceefae6bf608b
Benny Luo <bennyluo95@gmail.com>
2022-01-05 15:33:17 -0500

    added sections

--- src/App.js
+++ src/App.js
@@ -1,17 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const mainText = "Hi, I'm Benny"
+  const aboutText = "I am a problem solver..."
+
+  const buttonNames = [, "About"]
+
   return (
     <div className="App">
       <header className="App-header">
       </header>
-      <Body>
-        <div className=''>
-          <h3>Hi, I'm Benny</h3>
-          <img>ProfilePic1</img>
-        </div>
-      </Body>
+      <body>
+        <section className="mainSection">
+          <div>
+            <h3>{mainText}</h3>
+            <div>ProfilePic1</div>
+          </div>
+        </section>
+        <section className="aboutSection">
+          <div>
+            <h3>{aboutText}</h3>
+            <div>ProfilePic2</div>
+          </div>
+        </section>
+        <section className="projetsSection">
+          <h3>Projects</h3>
+          <div>
+            Alphatype
+          </div>
+          <div>
+            MKCharts
+          </div>
+        </section>
+        <section className="skillsSection">
+          <div>
+            <h3>Skills</h3>
+          </div>
+        </section>
+        <section className="contactSection">
+          <div>
+            <h3>Contact Me</h3>
+          </div>
+        </section>
+        <div className="buttonFlex">Buttons</div>
+
+      </body>
     </div>
   );
 }
