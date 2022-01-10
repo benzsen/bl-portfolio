23f17e59879d9247f8ab41404191cd0e174247c2
Benny Luo <bennyluo95@gmail.com>
2022-01-10 18:12:28 -0500

    set sections to flex

--- src/components/Body.js
+++ src/components/Body.js
@@ -8,47 +8,54 @@ const Body = () => {
       <div>
         <h1>Hi! I'm Benny</h1>
         <p>I am a web developer, engineer and problem solver.</p>
-        <div>
-          <img id="profilePic" src="./bl_photo(cropped).jpg" width="300"></img>
-        </div>
+        <img id="profilePic" src="./bl_photo_lake.jpg" width="300"></img>
       </div>
     </section>
     <section className="aboutSection">
-      <div className="aboutText">
-        <h3>About Me</h3>
-        <p>
-            I am a web developer with a background in mechanical engineering. 
-          <br/>
+      <div className="aboutFlex">
+        <img id="profilePic" src="./bl_photo(cropped).jpg" width="300"></img>
+        <div className="aboutText">
+          <h3>About Me</h3>
+          <p>
+            I am a web developer with a background in mechanical engineering.
+            <br/>
             My passion to build things with the purpose of solving problems sparked my curiosity to venture into software engineering.
-          <br/>
-            Through my self-teaching journey, I fell in love with web development. 
-        </p>
-        <p>Coming from the automotive industry (Toyota/Tesla), I am eager to bring a new perspective and a root-cause analysis mindset for troubleshooting and building clean code.</p>
-        <p>During my free time, I love playing PC co-op games, 3D printing and learning how to cook new recipes!</p>
-        <p></p>
-        <div>ProfilePic2</div>
+            Through my self-teaching journey, I fell in love with web development.
+          </p>
+          <p>Coming from the automotive industry (Toyota/Tesla), I am eager to bring a new perspective and a root-cause analysis mindset for troubleshooting and building clean code.</p>
+          <p>During my free time, I love playing PC co-op games, 3D printing and learning how to cook new recipes.</p>
+        </div>
       </div>
     </section>
-    <section className="projetsSection">
+    <section className="portfolioSection">
       <h3>Projects</h3>
-      <div>
-        <h4>alphaType</h4>
-        <span>This game will challenge you against the fastest alphabet typers in the world</span>
-        <br/>
-        <span>Sign up and see how your typing speed compares to others!</span>
-        <p>Javascript, HTML, CSS</p>
-        <p>MERN (MongoDB, Express, React, Node) </p>
+      <div className="ProjectFlex">
+        <img src="./alphaType_render.png" width="300px"></img>
+        <div className="projectDiv">
+          <h4>alphaType</h4>
+          <div>
+            <span>This game will challenge you against the fastest alphabet typers in the world</span>
+            <br/>
+            <span>Sign up and see how your typing speed compares to others!</span>
+            <p>Javascript, HTML, CSS, MERN (MongoDB, Express, React, Node)</p>
+          </div>
+        </div>
       </div>
-      <div>
-        <h4>MKCharts</h4>
-        <span>This app was made to track Mario Kart scores between my roomates and I</span>
-        <br/>
-        <span>My roomates and I were very competitive Mario Kart 8 players</span>
-        <br/>
-        <span>(PS- Mario Kart Settings: 4 races, Hard PCs, Frantic Items, Random Tracks)</span>
-        
-        <p>Javascript, HTML, CSS</p>
-        <p>MERN (MongoDB, Express, React, Node) </p>
+      <div className="ProjectFlex">
+        <img src="./MKCharts_render.png"></img>
+        <div className="projectDiv">
+          <h4>MKCharts</h4>
+          <div>
+            <span>This app allows user to input Mario Kart scores between my roomates and I</span>
+            <br/>
+            <span>Due to the competitive nature between my roomates and I, we needed a reliable tool to track our scores after each game.</span>
+            <p>Javascript, HTML, CSS, MERN (MongoDB, Express, React, Node) </p>
+          </div>
+        </div>
+      </div>
+      <div className="expDiv">
+        <h3>Experience</h3>
+        <div className="devExp"></div>
       </div>
     </section>
     <section className="skillsSection">
